c64cec67e95e46d11394a248a8ff04b10812c0b9
Giuseppe Mancone <manconeg@gmail.com>
2015-09-27 15:02:03 -0400

    refactored api functions to not be so convoluted

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 var assert = require("assert");
 
-describe('Array', function() {
-	describe('#indexOf()', function() {
+var Api = require("../lib/api");
+
+describe('Api', function() {
+	describe('#getOptions()', function() {
 		it('should return -1 when the value is not present', function() {
+      var api = new Api();
+      api.getOptions();
 			assert.equal(-1, [1, 2, 3].indexOf(5));
 			assert.equal(-1, [1, 2, 3].indexOf(0));
 		});
