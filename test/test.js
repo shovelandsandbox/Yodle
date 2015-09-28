c4cad55fb256bf937f79e256e045ed37e251463d
Giuseppe Mancone <manconeg@gmail.com>
2015-09-27 20:51:25 -0400

    passing tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,8 @@
 var assert = require("assert");
 
-var Api = require("../lib/api");
-
-describe('Api', function() {
-	describe('#getOptions()', function() {
+describe('Array', function() {
+	describe('#indexOf()', function() {
 		it('should return -1 when the value is not present', function() {
-      var api = new Api();
 			assert.equal(-1, [1, 2, 3].indexOf(5));
 			assert.equal(-1, [1, 2, 3].indexOf(0));
 		});
