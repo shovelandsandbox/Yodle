ec91ac25cd557d117918d54c63facf9fd207dcaa
Giuseppe Mancone <manconeg@gmail.com>
2015-09-30 22:25:32 -0400

    minor refactoring of directories

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,7 @@
 var assert = require("assert");
 var fs = require('fs');
 
-var Cli = require("../lib/commands/cli.js");
+var Cli = require("../lib/cli/index.js");
 var cli = new Cli();
 
 describe('Cli', function() {
